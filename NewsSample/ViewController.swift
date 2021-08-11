3ff44fac09d0e299f7ed1adb8bdc9ef89e957d01
MahmoudAllam <>
2021-08-11 14:58:38 +0200

    Added Pods swiftlint , swiftformat , kingfisher and alamofire also added git ignore

--- NewsSample/ViewController.swift
+++ NewsSample/ViewController.swift
@@ -9,13 +9,9 @@
 import UIKit
 
 class ViewController: UIViewController {
-
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         self.view.backgroundColor = .blue
     }
-
-
 }
-
--- NewsSampleTests/NewsSampleTests.swift
+++ NewsSampleTests/NewsSampleTests.swift
@@ -6,11 +6,10 @@
 //  Copyright © 2021 CircleHub. All rights reserved.
 //
 
-import XCTest
 @testable import NewsSample
+import XCTest
 
 class NewsSampleTests: XCTestCase {
-
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
@@ -30,5 +29,4 @@ class NewsSampleTests: XCTestCase {
             // Put the code you want to measure the time of here.
         }
     }
-
 }
