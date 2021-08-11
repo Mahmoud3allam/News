e42849d5af26f9575b770de2d4f5de10f4ecf2dc
MahmoudAllam <>
2021-08-11 14:46:54 +0200

    first commit after creating the project 🔥

---
+++ NewsSample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NewsSample
+//
+//  Created by Alchemist on 11/08/2021.
+//  Copyright © 2021 CircleHub. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
